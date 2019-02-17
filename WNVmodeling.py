d09813e0ab11f4198c69cc23cd7f3149229c7c92
Erolino <es3349@columbia.edu>
2019-02-17 02:26:51 -0500

    finished end of notebook

--- WNVmodeling.py
+++ WNVmodeling.py
@@ -35,6 +35,8 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_curve 
 from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import auc
 
 '''importing SMOTE '''
 from imblearn.over_sampling import SMOTE
@@ -52,6 +54,11 @@ smt=SMOTE(random_state=42)
 
 '''function for getting metrics for 3 different models for one dataset'''
 def models_metrics(Xn,Xt,yn,yt):
+    ###
+    maxfeat=int(np.round(np.sqrt(Xt.shape[1])))
+    maxdepth=int(np.round(maxfeat/3))
+    rfclf=RandomForestClassifier(n_estimators=500,max_depth=maxdepth,random_state=42,max_features=maxfeat)
+    ####
     models={'log_reg':log_reg,'knn':knn,'rf':rfclf}
     ss=pd.DataFrame()
     cols=[]
@@ -99,9 +106,119 @@ colsss=[list(sss.columns)[0]+'.'+list(datasets.keys())[0],list(sss.columns)[1]+'
         list(sss.columns)[9]+'.'+list(datasets.keys())[3],list(sss.columns)[10]+'.'+list(datasets.keys())[3],list(sss.columns)[11]+'.'+list(datasets.keys())[3]]
 
 sss.columns=colsss
-sss
+oy=sss.T
 
+#### ploting function for model performance depended on dataset 
+#### (plotting oy)
+import matplotlyb.pyplot as plt
+def plt_met(mett):
+    modls=['logistic Regression','K Nearest Neighbors','Random Forest Classifier']
+    colorS=['b','c','g','r']
+    metricss={'FN':0, 'FP':1, 'TN':2, 'TP':3, 'accuracy':4, 'aucroc':5, 'precision':6, 'recall':7}
+    plt.subplots(figsize=(23,3),)
+    for ai in range(3):
+        plt.xticks(rotation=20,fontsize = 12)
+        plt.subplot(1,3,ai+1)
 
+        indexx = range(4)
+        plt.plot(oy.iloc[[ai,ai+3,ai+6,ai+9],metricss[mett]],linestyle='None',marker='*',color=colorS[ai])
+        plt.xticks(indexx, oy.index[[ai,ai+3,ai+9,ai+6]])  # set the X ticks and labels
+
+        #plt.xticks(x, labels, rotation='vertical')
+        plt.xlabel('"dataset"',fontsize = 18)
+        plt.ylabel('metric: '+mett,fontsize = 18)
+        plt.title(modls[ai],color=colorS[ai])
+
+'''#################################'''
+
+'''Comparing day-of and 14-days with hyper parameter 
+    adjustments tuned for specific metrics( e.g. recall and aucroc )'''
+
+'''#################################'''
+
+### Grid Search
+# Although Random Forest Classifier is not better than Logistic Regression 
+# there are many hyper parameters that 
+# could be tuned to increase its perfomance.
+
+#Let's tune parametrs of the RF classifier towards higher recall. 
+# even on the expense of lowering precision (which is currently relatively high)
+
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import GridSearchCV,StratifiedKFold
+
+rfclf2=RandomForestClassifier(random_state=42)
+paramgrid={'max_depth':[2,3,5],'max_features':[4,8,16,20],
+           'min_samples_split':[2,3,8],'n_estimators':[200,300,500]}
+scorers={'accuracy_score':make_scorer(accuracy_score),
+         'precision_score':make_scorer(precision_score),
+         'recall_score':make_scorer(recall_score),
+         'aucroc_score':make_scorer(roc_auc_score)}
+
+def grid_func(the_score,jj): ## jj is the string name of the dataset
+    
+    #optimize hyper parametrs using grid search according to one of the scores that is input
+    skf=StratifiedKFold(n_splits=5) ## stratifying the crossvalidation and choosing the number of splits=10
+    GS=GridSearchCV(rfclf2,param_grid=paramgrid,scoring=scorers,refit=the_score,cv=skf,return_train_score='True',n_jobs=-1)
+    
+    ## choose dataset
+    Xtrain,Xtest,ytrain,ytest=train_test_split(datasets[jj].drop('WnvPresent',1),datasets[jj]['WnvPresent'],test_size=0.2,random_state=42,stratify=datasets[jj]['WnvPresent'])
+    Xtrain,ytrain=smt.fit_sample(Xtrain,ytrain)
+    #with parallel_backend('threading'):
+    GS.fit(Xtrain,ytrain)
+    
+    prediction=GS.predict(Xtest)
+    
+    print('best params for {}'.format(the_score))
+    print(GS.best_params_)
+    
+    print('confusion matrix adjusted for max {}'.format(the_score))
+    print(confusion_matrix(prediction,ytest))
+    
+    return(GS)
+    
+if 1==0:
+    GSrf2=grid_func('recall_score','sptrainW_day_of') ## for day-of input the 
+if 1==0:
+    GSrf2eng=grid_func('recall_score','sptrainW_14_day') ## for day-of input the 
+if 1==0:
+    GSrfAUC=grid_func('aucroc_score','sptrainW_day_of') ## for day-of input the 
+if 1==0:
+    GSrfAUCeng=grid_func('aucroc_score','sptrainW_14_day') ## for day-of input the 
+    
+
+if 1==0:
+    result=pd.DataFrame(GSrf2.cv_results_)
+    result.sort_values(['mean_test_aucroc_score','mean_test_recall_score'],inplace=True)
+    result[[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']].round(3).head()
+    ### the best auc score place in result:
+    roww0=np.where(result['mean_test_recall_score']==max(result['mean_test_recall_score']))
+    ### give the winning row for highest AUC:
+    winRecallresult=result.loc[roww0[0],[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']]
+    
+    resultEng=pd.DataFrame(GSrf2eng.cv_results_)
+    resultEng.sort_values(['mean_test_aucroc_score','mean_test_recall_score'],inplace=True)
+    resultEng[[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']].round(3).head()
+    ### the best auc score place in result:
+    roww1=np.where(resultEng['mean_test_recall_score']==max(resultEng['mean_test_recall_score']))
+    ### give the winning row for highest AUC:
+    winRecallresultEng=resultEng.loc[roww1[0],[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']]
+    
+    resultAuc=pd.DataFrame(GSrfAUC.cv_results_)
+    resultAuc.sort_values(['mean_test_aucroc_score','mean_test_recall_score'],inplace=True)
+    resultAuc[[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']].round(3).head()
+    ### the best auc score place in result:
+    roww2=np.where(resultAuc['mean_test_aucroc_score']==max(resultAuc['mean_test_aucroc_score']))
+    ### give the winning row for highest AUC:
+    winAucresult=resultAuc.loc[roww2[0],[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']]
+    
+    resultEngAuc=pd.DataFrame(GSrfAUCeng.cv_results_)
+    resultEngAuc.sort_values(['mean_test_aucroc_score','mean_test_recall_score'],inplace=True)
+    resultEngAuc[[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']].round(3).head()
+    ### the best auc score place in result:
+    roww3=np.where(resultEngAuc['mean_test_aucroc_score']==max(resultEngAuc['mean_test_aucroc_score']))
+    ### give the winning row for highest AUC:
+    winAucresultEng=resultEngAuc.loc[roww3[0],[ 'mean_test_aucroc_score','mean_test_recall_score','mean_test_precision_score', 'mean_test_accuracy_score','param_max_depth', 'param_max_features', 'param_min_samples_split', 'param_n_estimators']]
 
 '''#################################'''
 
